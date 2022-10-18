19e96bcc98edb654cc7769a2349457f90de55763
Tania <tania@MacBook-Pro-de-nico.local>
2022-10-19 00:48:59 +0200

    removed headers & replaced by 'cors: true'

--- back/src/main.ts
+++ back/src/main.ts
@@ -2,13 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
   });
   await app.listen(3000);
 }
